421374dc8f8c4e00dd2c2b4f04024f21d1cafb2e
quillotaku <ionutalexandru@hotmail.com>
2024-05-01 16:52:55 +0200

    added two options for the CLI, refactor some code and update README

--- README.md
+++ README.md
@@ -5,10 +5,16 @@ Script para sacar el máximo partido a la suscripción de Cinesa Unlimited
 1. Instalar los requirements `pip3 install -r requirements.txt`
 
 # Uso
-- `python3 cplanner.py CSV`
-- `CSV`: Nombre del fichero CSV (dentro de la ruta) para calcular la sesión máxima.
-  - Formato: Movie,Duration,Session
-  - Una linea por cada sesión disponible de la película.
-  - Se puede usar el script de `cscraper` para conseguir el csv mas fácilmente.
+- `python3 cplanner.py CSV [--exclude/include movie1,movie2,...,movieN]`
+  - `CSV`: Nombre del fichero CSV (dentro de la ruta) para calcular la sesión máxima.
+    - Formato: Movie,Duration,Session
+    - Una linea por cada sesión disponible de la película.
+    - Se puede usar el script de `cscraper` para conseguir el csv mas fácilmente.
+  - `--exclude`: OPCIONAL. Lista de películas separadas con coma para excluir del CSV (todas menos las seleccionadas). 
+  - `--include`: OPCIONAL. Lista de películas separadas con coma para incluir del CSV (solo las seleccionadas).
+  - Se usa argumentos posicionales:
+    - `CSV`: Posición 1.
+    - `--exclude`: Posición 2. Utilizar si no se usa `--include`.
+    - `--include`: Posición 2. Utilizar si no se usa `--exclude`.
 
-Hace un print de la las películas en orden y a que sesión hay que ver.
+Hace un print de la las películas en orden y a que sesión hay que ver. 
--- cplanner.py
+++ cplanner.py
@@ -3,59 +3,58 @@ from datetime import datetime, timedelta
 import sys
 from tabulate import tabulate
 from termcolor import colored
+import re
 
-# Assuming your CSV data is stored in a file named 'movies.csv'
-# You may need to adjust the file path accordingly
-# file_path = 'movies.csv'
-# print(sys.argv[1])
-
-# Read the CSV file into a DataFrame
-df = pd.read_csv(sys.argv[1])
-
-# Convert the 'Duration' column to timedelta format
-df['Duration'] = pd.to_timedelta(df['Duration'])
-
-# Convert the 'Session' column to datetime format
-df['Session'] = pd.to_datetime(df['Session'], format='%H:%M:%S')
-
-# add 10 minutes to df['Duration']
-df['Duration'] = df['Duration'] + timedelta(minutes=10)
-
-# Sort the DataFrame by session start times
-df_sorted = df.sort_values(by='Session')
+# check included or excluded movies with regex to ease the input on the user
+def check_movie(movie, patterns):
+    for pattern in patterns:
+        if re.search(pattern, movie, re.IGNORECASE):
+            return True
+    return False
 
 # Initialize variables
 selected_movies = []
-end_time = datetime.min
-
-# Initialize a list to store the selected movie sessions
+excluded_movies = []
+included_movies = []
 selected_sessions_list = []
+end_time = datetime.min
+try:
+    if sys.argv[2] == '--exclude':
+        excluded_movies = sys.argv[3].split(',')
+    elif sys.argv[2] == '--include':
+        included_movies = sys.argv[3].split(',')
+except:
+    pass
 
-# Iterate through the sorted DataFrame and select non-overlapping movies
+# Read the CSV file into a DataFrame, convert the 'Duration' column to timedelta format, convert the 'Session' column to datetime format
+# add 10 minutes to df['Duration'] for the trailers before the movie and sort the DataFrame by session start times
+df = pd.read_csv(sys.argv[1])
+df['Duration'] = pd.to_timedelta(df['Duration'])
+df['Session'] = pd.to_datetime(df['Session'], format='%H:%M:%S')
+df['Duration'] = df['Duration'] + timedelta(minutes=10)
+df_sorted = df.sort_values(by='Session')
+
+# Iterate through the sorted DataFrame and select non-overlapping movies and include or exclude movies depending on the variables initialized
 for index, row in df_sorted.iterrows():
     start_time = row['Session']
     movie_duration = row['Duration']
-    if start_time >= end_time and row['Movie'] not in selected_movies:
-        selected_movies.append(row['Movie'])
-        selected_sessions_list.append(row[['Movie', 'Session', 'Duration']])
-        end_time = start_time + movie_duration
-    else:
-        # print(f"Skipping {row['Movie']} at {start_time}, overlaps with the previous selection or already selected.")
-        pass
+    if (not included_movies or check_movie(row['Movie'],included_movies)) and not check_movie(row['Movie'],excluded_movies):
+        if start_time >= end_time and row['Movie'] not in selected_movies:
+            selected_movies.append(row['Movie'])
+            selected_sessions_list.append(row[['Movie', 'Session', 'Duration']])
+            end_time = start_time + movie_duration
 
-# Create a DataFrame from the list of selected movie sessions
+
+# Create a DataFrame from the list of selected movie sessions, Duration and Session in the format HH:MM:SS
 selected_sessions_df = pd.DataFrame(selected_sessions_list)
 selected_sessions_df['Session'] = selected_sessions_df['Session'].dt.strftime('%H:%M:%S')
 selected_sessions_df['Duration'] = pd.to_timedelta(selected_sessions_df['Duration'])
 selected_sessions_df['Duration'] = selected_sessions_df['Duration'].apply(lambda x: f"{x.components.hours:02}:{x.components.minutes:02}:{x.components.seconds:02}")
 
-
+#Copy the selected_sessions to add color just to print it in a nice way
 c_selected_sessions_df = selected_sessions_df.copy()
-
-
 headers = c_selected_sessions_df.columns
 c_selected_sessions_df.columns = [colored(header, 'cyan') for header in headers]
-
 for column in c_selected_sessions_df.columns:
     c_selected_sessions_df[column] = c_selected_sessions_df[column].apply(lambda x: colored(x, 'yellow'))
 
